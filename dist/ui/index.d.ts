a8de77fd322d9162cd8e4c4a4fb2eee769539dba
Maatq1544 <152024143+Maatq1544@users.noreply.github.com>
2026-03-27 03:08:28 +0300

    Add dist/ui/index.d.ts

---
+++ dist/ui/index.d.ts
@@ -0,0 +1,4 @@
+declare function TerminalTab(_props: unknown): import("react/jsx-runtime").JSX.Element;
+export { TerminalTab as default };
+export { TerminalTab };
+//# sourceMappingURL=index.d.ts.map
